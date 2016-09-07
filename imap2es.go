d9db6866f0dff497a1345b269e181e2f2d3f32a2
Enrico Bianchi <enrico.bianchi@par-tec.it>
2016-09-07 11:39:45 +0200

    Added options

--- imap2es.go
+++ imap2es.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gaal/go-options/options"
+	"os"
+)
+
+const VERSION = "0.0.0"
+
+const SPEC = `
+An IMAP to Elasticsearch indexer
+Usage: imap2es [OPTIONS]
+--
+h,help                Print this help
+v,version             Print version
+c,cfg=                Set the configuration file
+`
 
 func main() {
-    fmt.Println("Hello World")
+	s := options.NewOptions(SPEC)
+
+	// Check if options isn't passed
+	if len(os.Args[1:]) <= 0 {
+		s.PrintUsageAndExit("No option specified")
+	}
+	opts := s.Parse(os.Args[1:])
+
+	// Print version and exit
+	if opts.GetBool("version") {
+		fmt.Println("imap2es " + VERSION)
+		os.Exit(0)
+	}
 }
